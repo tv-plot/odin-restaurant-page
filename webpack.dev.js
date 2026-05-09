3f3ca552d35538cbc99812cf5ff70244c53f6d6c
Alexander Kamornikov <akamornikov@gmail.com>
2026-05-09 16:41:13 +0200

    Use webpack-merge for different webpack modes

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,38 +1,10 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "node:path";
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
 
-export default {
+export default merge(common, {
     mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-}
-
+    },    
+});
